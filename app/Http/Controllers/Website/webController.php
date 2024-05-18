fae87cc046bd72c99e4739518316e993e180220c
Rwiron <ruzindanawiron@gmail.com>
2024-05-18 15:45:25 +0200

    Frontend Work

--- app/Http/Controllers/Website/webController.php
+++ app/Http/Controllers/Website/webController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Website;
 
 use App\Http\Controllers\Controller;
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class webController extends Controller
 {
     public function index()
     {
-        return view('frontend.home');
+        // Fetch active rooms
+        $rooms = Room::all();
+        return view('frontend.home', compact('rooms'));
     }
 }
